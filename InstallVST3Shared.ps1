3098db32fdfecfb31698dd4043bfb1455f0dbdf6
Per Ivar Nerseth <perivar@nerseth.com>
2023-02-26 19:49:19 +0100

    Added shared vst3 and bassline

---
+++ InstallVST3Shared.ps1
@@ -0,0 +1,128 @@
+# set script parameters here
+param (
+    [Parameter(Mandatory = $false, Position = 0, Helpmessage = "Do you want to uninstall? [Y/N]")]
+    [ValidateSet(0, 1, 'true', 'false', 'yes', 'no', 'y', 'n', 'on', 'off', 'enabled', 'disabled')]
+    [String]$uninstall = 'false',
+
+    # make sure to add the scriptStatus parameter to ensure we do not elevate in never-ending loop
+    [Parameter(Mandatory = $false, Position = 1)]
+    [String]$scriptStatus 
+)
+
+# output if using -Verbose
+$Verbose = [bool]$PSCmdlet.MyInvocation.BoundParameters.ContainsKey("Verbose")
+if ($Verbose) {
+    Write-Verbose "-Verbose flag found on $($PSVersionTable.Platform)"
+}
+
+# output if using -Debug
+$Debug = [bool]$PSCmdlet.MyInvocation.BoundParameters.ContainsKey("Debug")
+if ($Debug) {
+    Write-Debug "-Debug flag found on $($PSVersionTable.Platform)"
+}
+
+# ############### #
+#                 #
+# START OF SCRIPT #
+#                 #
+# ############### #
+
+# Include the CommonFunctions.ps1 file
+. (Join-Path $PSScriptRoot CommonFunctions.ps1)
+
+if (!$(GetElevation)) {
+
+    # check status of relaunch so that we do not run in a never-ending loop
+    # if scriptStatus is RELAUNCHING, this means we were not able to elevate to Admin
+    # Write-Host "Script status: ${scriptStatus}" -ForegroundColor Yellow
+    if (($scriptStatus -eq "RELAUNCHING")) {
+        Write-Error "$($MyInvocation.MyCommand.Name) failed relaunching with admin rights, exiting ..."
+        exit
+    }
+    
+    Write-Error "$($MyInvocation.MyCommand.Name) is not running as Administrator, attempting to elevate..."
+    
+    $argumentsList = @(
+        '-NoExit'
+        '-File'
+        '"' + $MyInvocation.MyCommand.Definition + '"'
+        $uninstall
+        "RELAUNCHING"
+        $Debug ? "-Debug" : $null
+        $Verbose ? "-Verbose" : $null
+    )
+
+    ExecuteElevation $argumentsList
+}
+
+# ###################################################################
+#
+# The following runs only after the script is re-launched as elevated.
+#                
+# ###################################################################
+
+# Convert script parameter to boolean
+$doUninstall = ParseBool $uninstall
+Write-Host "Parameter found: uninstall: $doUninstall" -ForegroundColor Green
+
+# Include the GetEnvironment.ps1 file
+. (Join-Path $PSScriptRoot GetEnvironment.ps1)
+$environment = GetEnvironmentVariables "OneDrive"
+
+# define paths
+$source = Join-Path $environment.commonProgramFiles "VST3" "Shared"
+$target = Join-Path $environment.cloudHomeDir "Audio" "VstPlugins VST3"
+
+Write-Host ""
+Write-Host "Directory Paths:" -ForegroundColor Magenta
+Write-Host "source: $source" -ForegroundColor Magenta
+Write-Host "target: $target" -ForegroundColor Magenta
+Write-Host ""
+
+# +-----------------------+-----------------------------------------------------------+
+# | mklink syntax         | PowerShell equivalent                                     |
+# +-----------------------+-----------------------------------------------------------+
+# | mklink Link Target    | New-Item -ItemType SymbolicLink -Name Link -Target Target |
+# | mklink /D Link Target | New-Item -ItemType SymbolicLink -Name Link -Target Target |
+# | mklink /H Link Target | New-Item -ItemType HardLink -Name Link -Target Target     |
+# | mklink /J Link Target | New-Item -ItemType Junction -Name Link -Target Target     |
+# +-----------------------+-----------------------------------------------------------+
+#
+# https://www.delftstack.com/howto/powershell/create-symbolic-links-in-powershell/
+#
+# Examples:
+# DELETE:   (Get-Item C:\SPI).Delete() 
+# ADD:      New-Item -ItemType SymbolicLink -Path C:\SPI -Target "C:\Users\Chino\Dropbox (Reserve Membership)\SPI"
+
+if (Test-Path -Path $source -PathType Container) {
+
+    Write-Warning "Folder '${source}' already exist."
+
+    if ($doUninstall) {
+        $answer = "Y"
+    }
+    else {
+        $answer = GetYN "Do you want to delete the source directory? (Y/N)"
+    }
+
+    if ($answer -eq "Y") {
+        Write-Host "We are proceeding to delete the source directory" -ForegroundColor Cyan
+        Write-Host "Removing the folder: '${source}' ..." -ForegroundColor Cyan
+
+        (Get-Item ${source}).Delete() 
+    }
+    elseif ($answer -eq "N") {
+        Write-Host "You selected NO, exiting ..." -ForegroundColor Cyan
+        exit
+    }
+
+}
+else { 
+    Write-Warning "The folder '${source}' does not exist."
+
+    if (!$doUninstall) {
+        Write-Host "We are proceeding to add a symbolic link to the target directory" -ForegroundColor Cyan
+        New-Item -ItemType SymbolicLink -Path $source -Target $target
+    }
+}
+
